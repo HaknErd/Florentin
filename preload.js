e46ffa9e348a1969a97ef61fba88b412b8c8df18
Gogs <gogs@fake.local>
2023-02-17 18:51:26 +0100

    performance

---
+++ preload.js
@@ -0,0 +1,8 @@
+function preloadLinks() {
+    var links = document.getElementsByTagName('a');
+    for (var i = 0; i < links.length; i++) {
+        var xhr = new XMLHttpRequest();
+        xhr.open('GET', links[i].href, true);
+        xhr.send();
+    }
+}
